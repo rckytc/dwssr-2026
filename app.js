119170ab3098cc7f2b0900454c3c1a64341dfda9
rckytc <144040402+rckytc@users.noreply.github.com>
2026-09-18 18:14:48 +0000

    ✨ Feat: Enhance app.js Enhance app.js With middleware configuration and error handling comments; update www for debug message; add greet script in package.json; include itgam image

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
+//FUNCION PARA MANEJAR ERRORES
 var createError = require('http-errors');
+//IMPORTA EL FRAMEWORK EXPRESS
 var express = require('express');
+//IMPORTA EL MODULO PATH PARA MANEJAR RUTAS
 var path = require('path');
+//IMPORTA EL MODULO PARA MANEJAR COOKIES
 var cookieParser = require('cookie-parser');
+//IMPORTA EL MODULO PARA MANEJAR LOGS
 var logger = require('morgan');
-
+//IMPORTA EL MODULO PARA MANEJAR RUTAS DE LA APLICACION
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+//CREA UNA INSTANCIA DE EXPRESS (LA APLICACION) 
 var app = express();
 
-// view engine setup
+//CONFIGURACION DE MOTOR DE VISTAS (HANDLEBARS)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+//CONFIGURACION DE MIDDLEWARES
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//CONFIGURACION DE ARCHIVOS ESTATICOS (PUBLIC)
 app.use(express.static(path.join(__dirname, 'public')));
-
+//CONFIGURACION DE RUTAS(REGISTRAMOS)
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// CAPTURA DE ERRORES 404 Y ENVIO AL MANEJADOR DE ERRORES
 app.use(function(req, res, next) {
   next(createError(404));
 });
@@ -33,7 +40,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // RENDERIZA LA PAGINA DE ERROR CON EL ESTATUS DEL ERROR
   res.status(err.status || 500);
   res.render('error');
 });
