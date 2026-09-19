51991332382301a0be35d12a8b0f4bd8cf80582b
rckytc <144040402+rckytc@users.noreply.github.com>
2026-09-19 03:21:08 +0000

    ✨ Feat: Implementar estructura básica de la aplicación Express con rutas y manejo de errores

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //CONFIGURACION DE ARCHIVOS ESTATICOS (PUBLIC)
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'..','public')));
 //CONFIGURACION DE RUTAS(REGISTRAMOS)
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
